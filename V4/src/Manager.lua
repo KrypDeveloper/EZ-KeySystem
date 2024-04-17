e7e04cac8581e1c196d79db62c247ca58ae19da5
krypton <71219830+KrypDeveloper@users.noreply.github.com>
2024-04-17 12:02:55 -0300

    Update Manager.lua

--- V4/src/Manager.lua
+++ V4/src/Manager.lua
@@ -3,8 +3,9 @@ Module = {}
 local function a(Theme)
   return "https://raw.githubusercontent.com/KrypDeveloper/EZ-KeySystem/main/V4/Themes/".. Theme.. ".lua"
 end
+
 function Module.GetSelector(Theme)
-  if Theme ~= nil
+  if Theme ~= nil then
     local link = a(Theme)
     return loadstring(game:HttpGet(link))()
   else
