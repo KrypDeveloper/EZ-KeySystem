ebf3837c4cacdee626ce0570e5a7bc11fd94822c
krypton <71219830+KrypDeveloper@users.noreply.github.com>
2024-05-19 20:33:55 -0300

    Update EzKeySystemMain.lua

--- EzKeySystemMain.lua
+++ EzKeySystemMain.lua
@@ -3,29 +3,7 @@ local KeySys = loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDe
 Module = {}
 
 function Module.Main(tabela)
-KeySys.Main({
-  HubName = tabela.HubName,
-  PandaAuth = loadstring(game:HttpGet('https://raw.githubusercontent.com/Panda-Repositories/PandaKS_Libraries/main/library/LuaLib/ROBLOX/PandaBetaLib.lua'))(),
-  Discord = "https://discord.com/invite/txqzuUuTZh",
-  Service = tabela.Service,
-  CustomTextBoxMessage = nil, --put a string if u want
-  KeylessSettings = {
-  	LoopKeyless = {
-  	  Active = true,
-        Interval = 5
-       },
-        CorrectHWID = "Loading Script...",
-        IncorrectHWID = "INCORRECT HWID",
-       PremiumEnabled = false,
-       SavePremium = true 
-   },
-  NormalScript = function()
-    tabela.NormalScript()
-  end,
-  PremiumScript = function()
-    print("this is a premium script")
-  end
-})
+game.Players.LocalPlayer:Kick("Deprecated wait for Owner update")
 end
 
 return Module
