7d2d059f0adbd2ba4c51c4020712d9aa79e72e84
RIOT <71219830+KrypDeveloper@users.noreply.github.com>
2024-11-30 17:36:15 -0300

    Fixed syntax erros

--- V5/example.lua
+++ V5/example.lua
@@ -1,30 +1,28 @@
 local Settings = {
     Type = "Key",
-    UI = "MaggixV2"
+    UI = "MaggixV2",
     HubName = "Quasar Hub",
     Discord = "https://discord.com/invite/ugfag6bq",
     Blur = false
 }
 
-local Type = "Key"
-local UI = "MaggixV2"
-local KeySys = loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/EZ-KeySystem/main/V5/UIS/" .. Settings Type .. "/" .. Settings.UI .. ".lua"))()
+local KeySys = loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/EZ-KeySystem/main/V5/UIS/" .. Settings.Type .. "/" .. Settings.UI .. ".lua"))()
 
 local Validate = function(key)
     print(key)
-    return true
+    return false
 end
 
-if isfile("Pelinda Data/".. Settings.HubName.. "/Key.txt") and Validate(readfile("Pelinda Data/".. Settings.HubName.. "/Key.txt")) then
+if isfile("Pelinda Data/" .. Settings.HubName .. "/Key.txt") and Validate(readfile("Pelinda Data/" .. Settings.HubName .. "/Key.txt")) then
     print("Validated!")
 else
     local Window =
         KeySys:CreateGui(
         {
             Data = {
-                HubName = HubName
+                HubName = Settings.HubName
             },
-            Discord = Settings.Discord
+            Discord = Settings.Discord,
             Blur = Settings.Blur
         }
     )
@@ -39,7 +37,7 @@ else
         function(key)
             if Validate(key) then
                 print("Whitelisted")
-                writefile("Pelinda Data/".. Settings.HubName.. "/Key.txt", key)
+                writefile("Pelinda Data/" .. Settings.HubName .. "/Key.txt", key)
                 Window:DestroyGui()
             end
         end
